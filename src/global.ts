738fff083a873dda03651b46c99fef4d91186618
Mohamed Goma <mohamed.goma.dev@gmail.com>
2026-07-02 15:15:10 +0300

    type: change  to

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,4 @@
-export type options = "x" | "o";
-export type holder = options | undefined;
+export type players = "x" | "o";
+export type holder = players | undefined;
 
 export const numberOfSquares = 9;
