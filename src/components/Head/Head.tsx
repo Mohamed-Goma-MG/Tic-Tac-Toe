9791ef7ac98d6ac9916db36dd11b21daa2f1d4f3
Mohamed Goma <mohamed.goma.dev@gmail.com>
2026-07-02 17:56:12 +0300

    update: add required contexts for head

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,16 +1,20 @@
+import { useGameContext } from "../../contexts/GameContext";
+import zeroBased from "../../utils/zeroBased";
 import style from "./style.module.css";
 
 export default function Head() {
+  const { xCounter, oCounter } = useGameContext();
+
   return (
     <div className={style.head}>
       <div className={`${style.player} ${style.playerX}`}>
         <span className="logo">X</span>
-        <span className={style.counter}>00</span>
+        <span className={style.counter}>{zeroBased(xCounter)}</span>
       </div>
 
       <div className={`${style.player} ${style.playerO}`}>
         <span className="logo">O</span>
-        <span className={style.counter}>00</span>
+        <span className={style.counter}>{zeroBased(oCounter)}</span>
       </div>
     </div>
   );
